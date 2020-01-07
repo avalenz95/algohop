43fe359a11afe2d9f31232f9a3709ce58d4f3ec1
ablades <audaris@gmail.com>
2020-01-06 19:52:33 -0800

    add node visualization

--- app.py
+++ app.py
@@ -18,78 +18,52 @@ def index():
     G.add_edges_from([(1, 2), (1, 3)])
 
     #provides number of nodes
-    G.number_of_nodes()
+    print(f"# Nodes: {G.number_of_nodes()}")
 
     #provides number of edges
-    G.number_of_edges()
-
-    edge_x = []
-    edge_y = []
+    print(f"# Edges: {G.number_of_edges()}")
+    edge_xpos = []
+    edge_ypos = []
     for edge in G.edges():
+        edge_xpos.append(edge[0])
+        edge_ypos.append(edge[1])
         print(f"edge: {edge}")
-        # x0, y0 = G.nodes[edge[0]]
-        # x1, y1 = G.nodes[edge[1]]
-        # edge_x.append(x0)
-        # edge_x.append(x1)
-        # edge_x.append(None)
-        # edge_y.append(y0)
-        # edge_y.append(y1)
-        # edge_y.append(None)
 
     edge_trace = go.Scatter(
-        x=edge_x, y=edge_y,
+        x=edge_xpos, y=edge_ypos,
         line=dict(width=4, color='#888'),
         hoverinfo='none',
         mode='lines')
 
-    node_x = []
-    node_y = []
+    node_xpos = []
+    node_ypos = []
     for node in G.nodes():
+        node_xpos.append(node)
+        node_ypos.append(node)
+
         print(f"node: {node}")
-        # x, y = G.nodes[node]['pos']
-        # node_x.append(x)
-        # node_y.append(y)
 
     node_trace = go.Scatter(
-        x=node_x, y=node_y,
+        x=node_xpos, y=node_ypos,
         mode='markers',
         hoverinfo='text',
         marker=dict(
-            showscale=True,
-            # colorscale options
-            #'Greys' | 'YlGnBu' | 'Greens' | 'YlOrRd' | 'Bluered' | 'RdBu' |
-            #'Reds' | 'Blues' | 'Picnic' | 'Rainbow' | 'Portland' | 'Jet' |
-            #'Hot' | 'Blackbody' | 'Earth' | 'Electric' | 'Viridis' |
-            colorscale='YlGnBu',
-            reversescale=True,
             color=[],
+            #handles size of nodes
             size=30,
-            colorbar=dict(
-                thickness=15,
-                title='Node Connections',
-                xanchor='left',
-                titleside='right'
-            ),
+            #handles width of nodes
             line_width=5))
 
-    node_adjacencies = []
-    node_text = []
-    for node, adjacencies in enumerate(G.adjacency()):
-        node_adjacencies.append(len(adjacencies[1]))
-        node_text.append('# of connections: '+str(len(adjacencies[1])))
-
-    node_trace.marker.color = node_adjacencies
-    node_trace.text = node_text
 
     fig = go.Figure(data=[edge_trace, node_trace],
              layout=go.Layout(
-                title='<br>Network graph made with Python',
+                title='',
                 titlefont_size=16,
                 showlegend=False,
                 hovermode='closest',
                 margin=dict(b=20,l=5,r=5,t=40),
                 annotations=[ dict(
-                    text="Python code: <a href='https://plot.ly/ipython-notebooks/network-graphs/'> https://plot.ly/ipython-notebooks/network-graphs/</a>",
+                    text="",
                     showarrow=False,
                     xref="paper", yref="paper",
                     x=0.005, y=-0.002 ) ],
@@ -99,7 +73,8 @@ def index():
     figure = fig.show()
 
 
-    return render_template('index.html',figure=figure)
+
+    return render_template('index.html', figure=figure)
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run(debug=True)
